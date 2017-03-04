0c74b50cafcdf78d489ff56a44ae2e00da08e7c1
Pablo Vallejo <pabloninety@gmail.com>
2017-03-04 10:17:51 -0500

    Create Provider model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117004521) do
+ActiveRecord::Schema.define(version: 20170304151532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20170117004521) do
     t.string   "billing_unit"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "simulators", force: :cascade do |t|
